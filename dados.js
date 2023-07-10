52cdc754b84b1e0e4212768d4bcb9307c3606116
Wesley Valmir Dieterich <wesleyd@cscl.local>
2023-07-10 14:29:50 -0300

    first commit

---
+++ dados.js
@@ -0,0 +1,19 @@
+const usuarios = `
+  [
+    {
+      "nome": "Admin",
+      "email": "admin@email.com",
+      "senha" : 1234
+    },
+    {
+      "nome": "Teste",
+      "email": "teste@email.com",
+      "senha" : 1452
+    },
+    {
+      "nome": "Josnei",
+      "email": "jsonei@email.com",
+      "senha" : 9874
+    }
+  ]
+`
